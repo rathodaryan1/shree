e44861ee3969d176155613af120ea32eef6ca46f
Rathod Aryan <aaryan.b.rathod99@gmail.com>
2025-06-22 13:45:55 +0530

    Fix: Added ScrollToTop to reset scroll on route change

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -16,6 +16,7 @@ import ScrollToTopbutton from "@/components/ScrollToTopbutton";
 import ScrollToHashElement from "@/components/ScrollToHashElement";
 import ScrollToTop from "@/components/ScrollToTop";
 
+
 function App() {
   // Replace with your actual Google Analytics Measurement ID when available
   const GA_MEASUREMENT_ID = import.meta.env.VITE_GA_MEASUREMENT_ID || '';
@@ -30,12 +31,7 @@ function App() {
         {/* Floating CTA buttons */}
         <FloatingCTA />
         
-        {/* Scroll to top button */}
-        <ScrollToTopbutton />
-
-        {/* Scroll to top  */}
-        <ScrollToTop />   
-
+        <ScrollToTop />
         
         <Header />
         <main className="flex-grow">
