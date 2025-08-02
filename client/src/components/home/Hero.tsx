2ab4d1dcee6f148545cc217d3c836e9183d02aa1
Rathod Aryan <aaryan.b.rathod99@gmail.com>
2025-08-02 17:45:29 +0530

    Initial commit

--- client/src/components/home/Hero.tsx
+++ client/src/components/home/Hero.tsx
@@ -7,7 +7,7 @@ export default function Hero() {
       <div className="absolute inset-0 bg-gradient-to-r from-black/80 to-black/40 z-10"></div>
       <div className="absolute inset-0 z-0">
         <img 
-          src="/background.avif" 
+          src="/Background.png" 
           alt="Fire safety system" 
           className="w-full h-full object-cover"
         />
--- server/index.ts
+++ server/index.ts
@@ -62,7 +62,7 @@ app.use((req, res, next) => {
   // It is the only port that is not firewalled.
 server.listen(
   {
-    port: 5002,
+    port: 5003,
     host: "0.0.0.0"
   },
   () => {
